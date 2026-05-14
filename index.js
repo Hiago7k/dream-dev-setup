fbc4007228099b1d41ec83f8c9243660a5c0f9f1
Hiago 7k <hiago7k@gmail.com>
2026-05-13 21:43:09 -0300

    criando modulo angular.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+angular.module('devSetup', []);
+angular.module('devSetup').controller('devSetupCtrl', function($scope){
+    $scope.msg = "Testando ctrl controller para view";
+})
 
 
+$scope.perifericos = [
+    {nome: "Teclado", preco: "123", link: "https//amazon"},
+    {nome: "Mouse", preco: "56", link: "kabum.com.br"},
+    {nome: "Headset", preco: "993", link: "pichau.com.br"},
+    {nome: "Mousepad", preco: "100", link: "terabyte.com.br"}
+];
