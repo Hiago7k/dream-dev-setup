486d70828c95f84926f59aace0b4d1f16a19d6f5
Hiago 7k <hiago7k@gmail.com>
2026-05-18 20:54:28 -0300

    fazendo logica dark mode

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 angular.module('devSetup', []);
-angular.module('devSetup').controller('devSetupCtrl', function($scope){
+angular.module('devSetup').controller('devSetupCtrl', function ($scope) {
 
 
     $scope.items = [
-    {nome: "Teclado", preco: "123", link: "https//amazon"},
-    {nome: "Mouse", preco: "56", link: "kabum.com.br"},
-    {nome: "Headset", preco: "993", link: "pichau.com.br"},
-    {nome: "Mousepad", preco: "100", link: "terabyte.com.br"}
-];
+        { nome: "Teclado", preco: "123", link: "https//amazon" },
+        { nome: "Mouse", preco: "56", link: "kabum.com.br" },
+        { nome: "Headset", preco: "993", link: "pichau.com.br" },
+        { nome: "Mousepad", preco: "100", link: "terabyte.com.br" }
+    ];
+
+
 })
 
 
